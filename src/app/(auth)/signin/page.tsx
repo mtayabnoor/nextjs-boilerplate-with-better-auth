5b926e8567402580bb4fd68e2d21366876543012
muno1623 <mtayabnoor@gmail.com>
2026-03-29 17:51:01 +0200

    chore: Format all code

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -97,7 +97,7 @@ export default function SignInPage() {
                     <div className="flex items-center">
                       <FieldLabel htmlFor="password">Password</FieldLabel>
                       <a
-                        href="/forget-password"
+                        href="/forgot-password"
                         className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                       >
                         Forgot your password?
